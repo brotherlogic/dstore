d04fd654bdbc1d4bb0bcc2883923c545735896c5
simon <brotherlogic@gmail.com>
2021-06-29 06:15:40 -0700

    Log fanout

--- dstore_cli/cli.go
+++ dstore_cli/cli.go
@@ -28,7 +28,7 @@ func main() {
 	}
 
 	//rand.Shuffle(len(all), func(i, j int) { all[i], all[j] = all[j], all[i] })
-	chosen := all[1]
+	chosen := all[0]
 	fmt.Printf("Writing to %v\n", chosen)
 	conn, err := grpc.Dial(fmt.Sprintf("%v:%v", chosen.Identifier, chosen.Port), grpc.WithInsecure())
 	if err != nil {
