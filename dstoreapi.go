0857d03b22cf44a66e01f9b6cfc430e0271b7f26
simon <brotherlogic@gmail.com>
2021-06-28 21:21:08 -0700

    Main dstore imp

---
+++ dstoreapi.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"strings"
+	"time"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/brotherlogic/dstore/proto"
+)
+
+//Read reads out some data
+func (s *Server) Read(ctx context.Context, req *pb.ReadRequest) (*pb.ReadResponse, error) {
+	//Push the hash if we don't have one
+	if req.GetHash() == "" {
+		req.Hash = s.translate[req.GetKey()]
+	}
+
+	dir, file := extractFilename(req.GetKey())
+	resp, err := s.readFile(dir, file, req.GetHash())
+
+	code := status.Convert(err)
+	if code.Code() != codes.OK && code.Code() != codes.NotFound {
+		return nil, err
+	}
+	hashMap := make(map[string]*pb.ReadResponse)
+	hashMap[resp.GetHash()] = resp
+	countMap := make(map[string]int)
+	countMap[resp.GetHash()] = 1
+	bestCount := 1
+	bestHash := resp.GetHash()
+
+	req.NoFanout = true
+	friends, err := s.FFind(ctx, "dstore")
+	if err == nil {
+		for _, friend := range friends {
+			conn, err := s.FDialSpecificServer(ctx, "dstore", friend)
+			if err == nil {
+				client := pb.NewDStoreServiceClient(conn)
+
+				read, err := client.Read(ctx, req)
+				if err == nil {
+					if _, ok := hashMap[read.GetHash()]; !ok {
+						hashMap[read.GetHash()] = read
+					}
+
+					countMap[read.GetHash()]++
+					if countMap[read.GetHash()] > bestCount {
+						bestCount = countMap[read.GetHash()]
+						bestHash = read.GetHash()
+					}
+				}
+				conn.Close()
+			}
+		}
+	}
+
+	//Let's get a consensus on the latest
+	retResp := hashMap[bestHash]
+	retResp.Consensus = float32(bestCount) / float32(len(friends))
+
+	return retResp, nil
+}
+
+//Write writes out a key
+func (s *Server) Write(ctx context.Context, req *pb.WriteRequest) (*pb.WriteResponse, error) {
+	// Keys should not start with a '/'
+	if strings.HasPrefix(req.GetKey(), "/") {
+		return nil, fmt.Errorf("keys should not start with a backslash: %v", req.GetKey())
+	}
+
+	h := sha256.New()
+	h.Write(req.GetValue().Value)
+	hash := fmt.Sprintf("%x", h.Sum(nil))
+
+	err := s.writeToDir(req.GetKey(), string(h.Sum(nil)), &pb.ReadResponse{
+		Hash:      hash,
+		Value:     req.GetValue(),
+		Timestamp: time.Now().Unix(),
+	}, "latest")
+	if err != nil {
+		return nil, err
+	}
+
+	count := 1
+	if !req.NoFanout {
+		friends, err := s.FFind(ctx, "dstore")
+		if err == nil {
+			req.NoFanout = true
+			for _, friend := range friends {
+				if !strings.Contains(friend, s.Registry.Identifier) {
+					conn, err := s.FDialSpecificServer(ctx, "dtore", friend)
+					if err == nil {
+						client := pb.NewDStoreServiceClient(conn)
+						client.Write(ctx, req)
+					}
+				}
+			}
+		}
+		return &pb.WriteResponse{Consensus: float32(count) / float32(len(friends))}, nil
+	}
+
+	return &pb.WriteResponse{}, nil
+}
---
+++ dstoreutils.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/brotherlogic/dstore/proto"
+)
+
+func extractFilename(key string) (string, string) {
+	val := strings.LastIndex(key, "/")
+	if val < 0 {
+		return "", key
+	}
+	return key[0 : val+1], key[val+1:]
+}
+
+func (s *Server) deleteFile(dir, file string) {
+	s.Log(fmt.Sprintf("Removing %v%v -> %v", dir, file, os.Remove(s.basepath+dir+file)))
+}
+
+func (s *Server) readFile(dir, key, hash string) (*pb.ReadResponse, error) {
+	data, err := ioutil.ReadFile(s.basepath + key + "." + hash)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, status.Errorf(codes.NotFound, err.Error())
+		}
+
+		return nil, err
+	}
+
+	result := &pb.ReadResponse{}
+	err = proto.Unmarshal(data, result)
+	if err != nil {
+		return nil, err
+	}
+	err = proto.Unmarshal(data, result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (s *Server) writeToDir(dir, file string, toWrite *pb.ReadResponse, lnfile string) error {
+
+	data, err := proto.Marshal(toWrite)
+	if err != nil {
+		return err
+	}
+
+	os.MkdirAll(s.basepath+dir, 0777)
+	err = ioutil.WriteFile(fmt.Sprintf("%v%v%v", s.basepath, dir, file), data, 0644)
+	if len(lnfile) > 0 && err == nil {
+		//Silent delete of existing symlink
+		os.Remove(fmt.Sprintf("%v%v%v", s.basepath, dir, lnfile))
+		err = os.Symlink(fmt.Sprintf("%v", file), fmt.Sprintf("%v%v%v", s.basepath, dir, lnfile))
+	}
+	return err
+}
