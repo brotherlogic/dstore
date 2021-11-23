e16e7f6342498da983e31b4de60dd2f5e672e32f
Simon Tucker <brotherlogic@gmail.com>
2021-11-23 14:59:16 -0800

    Move position

--- dstore.go
+++ dstore.go
@@ -59,8 +59,8 @@ func (s *Server) GetState() []*pbg.State {
 
 func (s *Server) runCleans() {
 	for !s.LameDuck {
-		s.Log(fmt.Sprintf("Starting clean: %v", len(s.cleans)))
 		time.Sleep(time.Minute)
+		s.Log(fmt.Sprintf("Starting clean: %v", len(s.cleans)))
 		for _, key := range s.cleans {
 			ctx, cancel := utils.ManualContext("dstore-clean-"+key, time.Minute)
 			err := s.cleanDir(ctx, key)
