263cd9ee1ab52acc3b6731abdb79aa212f5a7b6d
Simon Tucker <brotherlogic@gmail.com>
2022-07-29 16:52:16 -0700

    Adding experimental dstore client

---
+++ client/dstoreclient.go
@@ -0,0 +1,48 @@
+package dstore_client
+
+import (
+	"context"
+
+	pb "github.com/brotherlogic/dstore/proto"
+	pbgs "github.com/brotherlogic/goserver"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+type DStoreClient struct {
+	Gs              pbgs.GoServer
+	ReadResponseMap map[string][]byte
+	Test            bool
+}
+
+func (c *DStoreClient) Read(ctx context.Context, in *pb.ReadRequest, opts ...grpc.CallOption) (*pb.ReadResponse, error) {
+	if c.Test {
+		return &pb.ReadResponse{
+			Value:     &anypb.Any{Value: c.ReadResponseMap[in.GetKey()]},
+			Consensus: 1.0,
+		}, nil
+	}
+	conn, err := c.Gs.FDialServer(ctx, "dstore")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	client := pb.NewDStoreServiceClient(conn)
+	return client.Read(ctx, in, opts...)
+}
+
+func (c *DStoreClient) Write(ctx context.Context, in *pb.WriteRequest, opts ...grpc.CallOption) (*pb.WriteResponse, error) {
+	if c.Test {
+		c.ReadResponseMap[in.GetKey()] = in.GetValue().GetValue()
+		return &pb.WriteResponse{Consensus: 1.0}, nil
+	}
+	conn, err := c.Gs.FDialServer(ctx, "dstore")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	client := pb.NewDStoreServiceClient(conn)
+	return client.Write(ctx, in, opts...)
+}
