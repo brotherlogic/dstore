1e3a6e5051ee3d507724b07c5548a56c510f3a30
Simon Tucker <brotherlogic@gmail.com>
2022-08-07 10:14:20 -0700

    Adds test

--- dstoreutils_test.go
+++ dstoreutils_test.go
@@ -1,7 +1,9 @@
 package main
 
-func RunBasicTest(t *testing.T){
-    if true {
-        t.Errorf("Bad test")
-    }
+import "testing"
+
+func RunBasicTest(t *testing.T) {
+	if true {
+		t.Errorf("Bad test")
+	}
 }
