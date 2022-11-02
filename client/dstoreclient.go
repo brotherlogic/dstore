eaf5151d0faaf2f7d938236c9c75b85b5c46c8d6
Simon Tucker <brotherlogic@gmail.com>
2022-11-02 07:44:57 -0700

    Support error code return

--- client/dstoreclient.go
+++ client/dstoreclient.go
@@ -6,17 +6,24 @@ import (
 	pb "github.com/brotherlogic/dstore/proto"
 	pbgs "github.com/brotherlogic/goserver"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
 type DStoreClient struct {
 	Gs              *pbgs.GoServer
 	ReadResponseMap map[string][]byte
+	ErrorCode       map[string]codes.Code
 	Test            bool
 }
 
 func (c *DStoreClient) Read(ctx context.Context, in *pb.ReadRequest, opts ...grpc.CallOption) (*pb.ReadResponse, error) {
 	if c.Test {
+		if val, ok := c.ErrorCode[in.GetKey()]; ok {
+			return nil, status.Errorf(val, "Request error")
+		}
+
 		return &pb.ReadResponse{
 			Value:     &anypb.Any{Value: c.ReadResponseMap[in.GetKey()]},
 			Consensus: 1.0,
